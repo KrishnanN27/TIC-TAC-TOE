480ebc9c148b1938375804976715f13ae9e0416f
Krishna <krishnaofficial27@gmail.com>
2022-10-22 12:57:28 +0530

    Added logic to the game and displaying winner of the game

--- lib/main.dart
+++ lib/main.dart
@@ -5,67 +5,130 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  bool onTurn = true;
+  List displayEx0h = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: Colors.grey[800],
+      body: GridView.builder(
+          itemCount: 9,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                _tapped(index);
+              },
+              child: Container(
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.grey)),
+                child: Center(
+                  child: Text(
+                    displayEx0h[index],
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                ),
+              ),
+            );
+          }),
     );
+  }
+
+  void _tapped(int index) {
+    setState(() {
+      if (onTurn) {
+        displayEx0h[index] = 'o';
+      } else {
+        displayEx0h[index] = 'x';
+      }
+      onTurn = !onTurn;
+      _checkWinner();
+    });
+  }
+
+  void _checkWinner() {
+
+    //check first row
+    if (displayEx0h[0] == displayEx0h[1] &&
+        displayEx0h[0] == displayEx0h[2] &&
+        displayEx0h[0] != '') {
+      _showWinDialog(displayEx0h[0]);
+    }
+    else if (displayEx0h[3] == displayEx0h[4] &&
+        displayEx0h[3] == displayEx0h[5] &&
+        displayEx0h[3] != '') {
+      _showWinDialog(displayEx0h[3]);
+    }
+    else if (displayEx0h[6] == displayEx0h[7] &&
+        displayEx0h[6] == displayEx0h[8] &&
+        displayEx0h[6] != '') {
+      _showWinDialog(displayEx0h[6]);
+    }
+    else if (displayEx0h[0] == displayEx0h[3] &&
+        displayEx0h[0] == displayEx0h[6] &&
+        displayEx0h[0] != '') {
+      _showWinDialog(displayEx0h[0]);
+    }
+    else if (displayEx0h[1] == displayEx0h[4] &&
+        displayEx0h[1] == displayEx0h[7] &&
+        displayEx0h[1] != '') {
+      _showWinDialog(displayEx0h[1]);
+    }
+    else if (displayEx0h[2] == displayEx0h[5] &&
+        displayEx0h[2] == displayEx0h[8] &&
+        displayEx0h[2] != '') {
+      _showWinDialog(displayEx0h[2]);
+    }
+    else if (displayEx0h[6] == displayEx0h[4] &&
+        displayEx0h[6] == displayEx0h[2] &&
+        displayEx0h[6] != '') {
+      _showWinDialog(displayEx0h[6]);
+    }
+    else if (displayEx0h[0] == displayEx0h[4] &&
+        displayEx0h[0] == displayEx0h[8] &&
+        displayEx0h[0] != '') {
+      _showWinDialog(displayEx0h[0]);
+    }
+  }
+
+  void _showWinDialog(String winner) {
+    
+    showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(
+        title: Text('Winner is : ' + winner),
+      );
+    });
+    
   }
 }
